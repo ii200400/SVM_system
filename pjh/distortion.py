2fd813c3b3a592ff2839409a8abd08473bcc18e6
박주현 <jh1997@kakao.com>
2022-10-20 14:27:16 +0900

    [PJH] feat: 왜곡보정 이미지 추가 및 txt 파일 추가

--- pjh/distortion.py
+++ pjh/distortion.py
@@ -3,11 +3,24 @@ import sys # assert cv2.__version__[0] == '3', 'The fisheye module requires open
 import numpy as np
 import os
 import glob
-from regex import P
+# from regex import P
 
 # 체커보드 
-CHECKERBOARD = (6,8)
+# CHECKERBOARD = (6,8)
+CHECKERBOARD = (6,9)
+'''
+cv2.TERM_CRITERIA_EPS = 종료 조건의 타입으로 주어진 정확도(epsilon 인자)에 도달하면 반복을 중단
+cv2.TERM_CRITERIA_MAX_ITER = max_iter 인자에 지정된 횟수만큼 반복하고 중단
+cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER를 조합해 사용하면 두가지 조건 중 하나가 만족되면 반복이 중단
+'''
 subpix_criteria = (cv2.TERM_CRITERIA_EPS+cv2.TERM_CRITERIA_MAX_ITER, 30, 0.1)
+
+'''
+Calibration Flag 조합
+cv2.fisheye.CALIB_RECOMPUTE_EXTRINSIC = 외부는 내부 최적화를 반복할 때마다 다시 계산
+cv2.fisheye.CALIB_CHECK_COND = 함수는 조건 번호의 유효성을 확인
+cv2.fisheye.CALIB_FIX_SKEW = 스큐 계수(알파)는 0으로 설정되고 0을 유지
+'''
 calibration_flags = cv2.fisheye.CALIB_RECOMPUTE_EXTRINSIC+cv2.fisheye.CALIB_CHECK_COND+cv2.fisheye.CALIB_FIX_SKEW
 objp = np.zeros((1, CHECKERBOARD[0]*CHECKERBOARD[1], 3), np.float32)
 objp[0,:,:2] = np.mgrid[0:CHECKERBOARD[0], 0:CHECKERBOARD[1]].T.reshape(-1, 2)
@@ -16,21 +29,25 @@ objpoints = [] # 3d point in real world space1
 imgpoints = [] # 2d points in image plane.
 
 # 체커보드를 다각도에서 찍은 이미지들로부터 파라미터들을 찾는 과정
-images = glob.glob('./data/simulation_intrinsic/*.png')
+# images = glob.glob('./data/simulation_intrinsic/*.png')
+images = glob.glob('./data/test_images/*.jpg')
+print(images)
 for fname in images:    
     img = cv2.imread(fname)
     if _img_shape == None:
-        _img_shape = img.shape[:2]
+        _img_shape = img.shape[:2] # image.shape(:2) => img.shape(height, width)
     else:
-        assert _img_shape == img.shape[:2], "All images must share the same size."    
+        assert _img_shape == img.shape[:2], "All i same size."    
         gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     # 코너 찾기
         ret, corners = cv2.findChessboardCorners(gray, CHECKERBOARD, cv2.CALIB_CB_ADAPTIVE_THRESH+cv2.CALIB_CB_FAST_CHECK+cv2.CALIB_CB_NORMALIZE_IMAGE)
     # If found, add object points, image points (after refining them)
         if ret == True:
             objpoints.append(objp)
+            # print('objp' + str(objp))
             cv2.cornerSubPix(gray,corners,(3,3),(-1,-1),subpix_criteria)
             imgpoints.append(corners)
+            # print(imgpoints)
             N_OK = len(objpoints)
        
 K = np.zeros((3, 3))
@@ -72,11 +89,11 @@ def undistort(img_path):
 
     ## new_K 설정 
     new_K = K.copy()
-    new_K[0,0]=K[0,0]/1.5
-    new_K[1,1]=K[1,1]/1.5
+    new_K[0,0]=K[0,0]/3
+    new_K[1,1]=K[1,1]/3
 
     map1, map2 = cv2.fisheye.initUndistortRectifyMap(K, D, np.eye(3), new_K, DIM, cv2.CV_16SC2)
-    undistorted_img = cv2.remap(img, map1, map2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_REPLICATE)    
+    undistorted_img = cv2.remap(img, map1, map2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)    
     cv2.imshow("undistorted", undistorted_img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
@@ -85,7 +102,8 @@ print("K=np.array(" + str(K.tolist()) + ")")
 print("D=np.array(" + str(D.tolist()) + ")")
 
 ## 체커보드 이미지들 확인
-images = glob.glob('./data/simulation_intrinsic/*.png')
+# images = glob.glob('./data/simulation_intrinsic/*.png')
+images = glob.glob('./data/test_images/*.jpg')
 for fname in images:
     undistort(fname)
 
