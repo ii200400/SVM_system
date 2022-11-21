25751569b4174fdec7708a8f4f36e934e87ce70d
박주현 <jh1997@kakao.com>
2022-11-21 14:40:46 +0900

    [S07P31D108] 2D SurrounView 최종

--- 2d/camaraTool/all_capture.py
+++ 2d/camaraTool/all_capture.py
@@ -60,15 +60,15 @@ print('width4 :%d, height4 : %d' % (cap4.get(3), cap4.get(4)))
 num = 1
 while(True):
 
-    ret1, front = cap1.read()    # Read 결과와 frame
-    ret4, left = cap2.read()
-    ret3, right = cap3.read()
-    ret2, back = cap4.read()
+    ret1, frame_1 = cap1.read()    # Read 결과와 frame
+    ret2, frame_2 = cap2.read()
+    ret3, frame_3 = cap3.read()
+    ret4, frame_4 = cap4.read()
 
     if(ret1) :
         # cam1 = undi_top(frame1, 'right')         
-        cv2.imshow('front', front) 
-        undistorted_front = undistort(front, 1.5)
+        cv2.imshow('frame_1', frame_1) 
+        undistorted_front = undistort(frame_1, 1.5)
         # cv2.imshow('frame_1', undistorted_img1)     
 
         pass
@@ -76,32 +76,32 @@ while(True):
     if(ret2) :
         # cam2 = undi_top(frame2, 'front')   
         # cv2.imshow('frame_2_front', cam2)
-        cv2.imshow('back', back) 
-        undistorted_back = undistort(back, 1.5)
+        cv2.imshow('frame_2', frame_2) 
+        undistorted_back = undistort(frame_2, 1.5)
         # cv2.imshow('frame_2', undistorted_img2) 
         pass
 
     if(ret3) :
         # cam3 = undi_top(frame3, 'back')     
         # cv2.imshow('frame_3_back', cam3)
-        cv2.imshow('right', right) 
-        undistorted_right = undistort(right, 1.5)
+        cv2.imshow('frame_3', frame_3) 
+        undistorted_right = undistort(frame_3, 1.5)
         # cv2.imshow('frame_3', undistorted_img3) 
         pass
 
     if(ret4) :
-        cv2.imshow('left', left) 
-        undistorted_left = undistort_left(left, 1.5)
+        cv2.imshow('frame_4', frame_4) 
+        undistorted_left = undistort_left(frame_4, 1.5)
 
         
     
     key = cv2.waitKey(1)
 
     if key == ord('z'):
-        cv2.imwrite('front' + str(num) + '.png', front)
-        cv2.imwrite('back' + str(num) + '.png', back)
-        cv2.imwrite('right' + str(num) + '.png', right)
-        cv2.imwrite('left' + str(num) + '.png', left)
+        cv2.imwrite('front' + str(num) + '.png', frame_1)
+        cv2.imwrite('back' + str(num) + '.png', frame_2)
+        cv2.imwrite('right' + str(num) + '.png', frame_3)
+        cv2.imwrite('left' + str(num) + '.png', frame_4)
         cv2.imwrite('undistorted_front' + str(num) + '.png', undistorted_front)
         cv2.imwrite('undistorted_back' + str(num) + '.png', undistorted_back)
         cv2.imwrite('undistorted_right' + str(num) + '.png', undistorted_right)
